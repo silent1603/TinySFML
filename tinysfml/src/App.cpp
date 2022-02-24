baa801b435b8a4a1465345f94521b0482016a3f6
silent1603 <tamgiangbui@gmail.com>
2022-02-24 09:37:52 +0700

    Update configuration cmake for window

--- tinysfml/src/App.cpp
+++ tinysfml/src/App.cpp
@@ -1,5 +1,5 @@
 #include "App.hpp"
-
+#include <filesystem>
 namespace tinySFML
 {
     App::App()
@@ -15,12 +15,12 @@ namespace tinySFML
     void App::init()
     {
         tinySFML::Logger::Init();
-        TINYSFML_INFO("init app");
+        TINYSFML_INFO(std::filesystem::current_path().string());
         data = tinySFML::SceneIO::Load("./resources/data/data.dat");
         int w = data["Window"]["width"].get<int>();
         int h = data["Window"]["height"].get<int>();
         std::string title = data["Window"]["title"].get<std::string>();
-        m_windowhander =  std::make_unique<tinySFML::Window>("Window",800,600);
+        m_windowhander =  std::make_unique<tinySFML::Window>(title,w,h);
         m_windowhander->init();
     }
 
--- tinysfml/src/GameObject.hpp
+++ tinysfml/src/GameObject.hpp
@@ -5,75 +5,24 @@
 #include <SFML/System/Vector2.hpp>
 namespace tinySFML
 {
-    template <typename T>
+    
     class GameObject
     {
     public:
         GameObject();
-        virtual void pos(sf::Vector2i pos);
-        virtual void size(sf::Vector2u size);
-        virtual void color(sf::Color c);
-        virtual void color(uint8_t r, uint8_t g, uint8_t b);
-        virtual void move(int sx, int sy);
         ~GameObject();
+        virtual void pos(sf::Vector2i pos)  ;
+        virtual void size(sf::Vector2u size) ;
+        virtual void color(sf::Color c) ;
+        virtual void move(sf::Vector2i m_pos);
 
     private:
-        T m_type;
         sf::Vector2i m_pos;
         sf::Vector2u m_size;
         bool m_visible;
         sf::Color m_color{sf::Color::Black};
     };
 
-    template <>
-    void GameObject<sf::CircleShape>::pos(sf::Vector2i pos)
-    {
-    }
-
-    template <>
-    void GameObject<sf::RectangleShape>::pos(sf::Vector2i pos)
-    {
-    }
-
-    template <>
-    void GameObject<sf::CircleShape>::color(sf::Color c)
-    {
-    }
-
-    template <>
-    void GameObject<sf::CircleShape>::color(uint8_t r, uint8_t g, uint8_t b)
-    {
-    }
-
-    template <>
-    void GameObject<sf::RectangleShape>::color(uint8_t r, uint8_t g, uint8_t b)
-    {
-    }
-
-    template <>
-    void GameObject<sf::RectangleShape>::color(sf::Color c)
-    {
-    }
-
-    template <>
-    void GameObject<sf::CircleShape>::move(int sx, int sy)
-    {
-    }
-
-    template <>
-    void GameObject<sf::RectangleShape>::move(int sx, int sy)
-    {
-    }
-
-    template <>
-    void GameObject<sf::CircleShape>::size(sf::Vector2u size)
-    {
-    }
-
-    template <>
-    void GameObject<sf::RectangleShape>::size(sf::Vector2u size)
-    {
-    }
 
 }
 #endif
